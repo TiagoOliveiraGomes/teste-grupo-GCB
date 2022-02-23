ad4820e37ea2d7f114988d32f6495a1cf15669c3
TiagoOliveiraGomes1 <tiago.oliveira@teclat.com.br>
2022-02-23 13:08:54 -0300

    add input option

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -7,11 +7,15 @@ import HeaderOptions from '../headerOptions'
 const Header = () => {
     return (
         <div className='header'>
+            <img className='img' src={background} />
             <div className='topContain'>
                 <Tittle tittle="Healthy Food"/>
                 <HeaderOptions />
             </div>
-            <img className='img' src={background} />
+            <div>
+                <h1 className='inputQuestion'>Ready for Trying a new recipe?</h1>
+                <input type="text" />
+            </div>
         </div>
     )
 }
