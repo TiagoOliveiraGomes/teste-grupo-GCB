c6363e73553f6c99796e7e6fbade970715583ee3
TiagoOliveiraGomes1 <tiago.oliveira@teclat.com.br>
2022-02-25 16:32:43 -0300

    update carousel-component

--- src/components/body/bottom/index.jsx
+++ src/components/body/bottom/index.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import React from 'react'
 import './bodyBottom.css'
 import BearCarousel, {
     TBearSlideItemDataList,
@@ -6,26 +6,38 @@ import BearCarousel, {
   } from "bear-react-carousel";
   import 'bear-react-carousel/dist/index.css';
 import Card from '../../card'
-import imagemComida from '../../../assets/img/comida_1.svg'
+import blog1 from '../../../assets/img/blog_image_1.svg'
+import blog2 from '../../../assets/img/bloco_image_2.svg'
+import blog3 from '../../../assets/img/bloco_image_3.svg'
+import blog4 from '../../../assets/img/bloco_image_4.svg'
 
 const BodyBottom = () => {
 
     const images = [
-        { id: 1, image: imagemComida },
-        { id: 2, image: imagemComida },
-        { id: 3, image: imagemComida }
+        { id: 1, image: blog1, text: "Quick-start guide to nuts and seeds" },
+        { id: 2, image: blog2, text: "Nutrition: Tips for Improving Your Health" },
+        { id: 3, image: blog3, text: "The top 10 benefits of eating healthy" },
+        { id: 4, image: blog4, text: "What is More Healthy?" },
       ];
+      // const bearSlideItemData = images.map((row) => {
+      //   return {
+      //     key: row.id,
+      //     children:<BearSlideItem>
+      //           <div className="container-card">
+      //               <img className="img-card" src={row.image}/>
+      //               <div className="button-group">
+      //                   <div className="text-card">texto</div>
+      //                   <button>See Recipe</button>
+      //               </div>
+      //           </div>
+      //     </BearSlideItem> 
+      //   };
+      // });
       const bearSlideItemData = images.map((row) => {
         return {
           key: row.id,
           children:<BearSlideItem>
-                <div className="container-card">
-                    <img className="img-card" src={row.image}/>
-                    <div className="button-group">
-                        <div className="text-card">texto</div>
-                        <button>See Recipe</button>
-                    </div>
-                </div>
+                <Card source={row.image} text={row.text} />
           </BearSlideItem> 
         };
       });
@@ -38,14 +50,14 @@ const BodyBottom = () => {
             </div>
             <div className='test-square'>
                 <BearCarousel
-                
+                className='carosel'
                 data={bearSlideItemData}
                 enableAutoPlay
-                autoPlayTime={3000}
+                autoPlayTime={100}
                 isEnableLoop
                 isEnableNavButton
                 isEnablePagination
-                slidesPerView={2}
+                slidesPerView={4}
                 // isDebug
                 aspectRatio={{ widthRatio: 16, heightRatio: 9 }}
                 />
