7ee249a6a2995cd1830f05aab23f9f8fccc4c948
TiagoOliveiraGomes1 <tiago.oliveira@teclat.com.br>
2022-02-25 08:37:16 -0300

    initial block-blog

--- src/components/body/bottom/index.jsx
+++ src/components/body/bottom/index.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import './bodyBottom.css'
 
 const bodyBottom = () => {
     return (
-        <div></div>
+        <div className='body-Bottom'>
+            <div className='initial-texts'>
+                <h1 className='tittle'>Read Our Blog</h1>
+                <h3 className='text'>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</h3>
+            </div>
+            <div className='test-square' />
+        </div>
     )
 }
 
