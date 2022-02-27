9753c731ceee4d8eb30f88bb6de6d5620d24e184
TiagoOliveiraGomes1 <tiago.oliveira@teclat.com.br>
2022-02-27 15:06:30 -0300

    fix 1
    fix menu-toggle, add click in Logo-page to return to home-page

--- src/components/navHeader/index.jsx
+++ src/components/navHeader/index.jsx
@@ -1,12 +1,16 @@
 import React from "react";
+import './navHeader.css'
 import HeaderOptions from "../headerOptions";
 import MenuToggle from "../menuToggle";
 import Tittle from "../tittle";
+import { Link } from "react-router-dom";
 
 const NavHeader = () => {
   return (
     <div className="topContain">
-      <Tittle tittle="Healthy Food" />
+      <Link to="/" className="button">
+        <Tittle tittle="Healthy Food" />
+      </Link>
       <div className="menu-section">
         <MenuToggle />
         <HeaderOptions />
