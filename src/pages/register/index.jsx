27f2884b2bb36a556639f1d48b3fb652bb891736
TiagoOliveiraGomes1 <tiago.oliveira@teclat.com.br>
2022-03-03 21:21:36 -0300

    todo

--- src/pages/register/index.jsx
+++ src/pages/register/index.jsx
@@ -6,6 +6,7 @@ import Form from '../../components/form';
 const Register = () => {
     return (
         <div className='Container-register'>
+            {/* TODO: Consertar validação de CPF (numero válido) */}
             <NavHeader/>
             <Form />
         </div>
